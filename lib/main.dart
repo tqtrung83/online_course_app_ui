aa6d83b52b6c547971a0545ca1f44f174fdff191
Ryan Trinh <tqtrung83@gmail.com>
2020-04-24 20:44:58 -0230

    Updated HomeScreen

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Online Course App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:onlinecourseappui/models/category.dart';
+import 'package:onlinecourseappui/screens/constants.dart';
+
+import 'detail_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -8,6 +14,137 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(left: 20, top: 50, right: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                SvgPicture.asset('assets/icons/menu.svg'),
+                Image(
+                  image: AssetImage('assets/images/user.png'),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Hey Alex',
+              style: kHeadingextStyle,
+            ),
+            Text(
+              'Find a course that you want to learn',
+              style: kSubheadingextStyle,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              height: 60,
+              margin: EdgeInsets.symmetric(vertical: 30),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Color(0xFFF5F5F7),
+                borderRadius: BorderRadius.circular(40),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      SvgPicture.asset('assets/icons/search.svg'),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Text(
+                        'Search',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Color(0xFFA0A5BD),
+                        ),
+                      ),
+                    ],
+                  ),
+                  IconButton(
+                    icon: Icon(
+                      Icons.clear,
+                      size: 20,
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'Categories',
+                  style: kHeadingextStyle,
+                ),
+                Text(
+                  'See All',
+                  style: kSubtitleTextSyule.copyWith(color: kBlueColor),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Expanded(
+              child: StaggeredGridView.countBuilder(
+                  padding: EdgeInsets.all(0),
+                  crossAxisCount: 2,
+                  itemCount: categories.length,
+                  mainAxisSpacing: 20,
+                  crossAxisSpacing: 20,
+                  itemBuilder: (context, index) {
+                    Category category = categories[index];
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).push(
+                            MaterialPageRoute(builder: (_) => DetailScreen()));
+                      },
+                      child: Container(
+                        height: index.isEven ? 200 : 240,
+                        decoration: BoxDecoration(
+                          color: kBlueColor,
+                          borderRadius: BorderRadius.circular(16),
+                          image: DecorationImage(
+                              image: AssetImage(category.image),
+                              fit: BoxFit.cover),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                category.name,
+                                style: kTitleTextStyle,
+                              ),
+                              Text(
+                                '${category.numOfCourses} Courses',
+                                style: TextStyle(
+                                  color: kTextColor.withOpacity(0.5),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                  staggeredTileBuilder: (index) => StaggeredTile.fit(1)),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
